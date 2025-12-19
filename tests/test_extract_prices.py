7cf568dddabf1b1b9d2e3513c942d755fe14b704
barbara.carvalho <barbara.carvalho@olx.com>
2025-12-19 17:58:44 +0000

    Fixing import issue

--- tests/test_extract_prices.py
+++ tests/test_extract_prices.py
@@ -1,5 +1,8 @@
-import pytest
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from src.extract_prices import *
+
 
 def test_save_to_json():
     """Test saving events creates a file and writes correctly."""
